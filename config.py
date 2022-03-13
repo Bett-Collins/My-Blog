add213138f2d28d17d6a82ae153471bdccb8ac70
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-13 15:11:05 +0300

    it will setup configuration

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
 UPLOADED_PHOTOS_DEST ='app/static/photos'
+#  email configurations
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 

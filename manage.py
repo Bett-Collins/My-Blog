73c5f1698ad88b153fa7aaf122a24bdd7a199f59
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-13 12:23:35 +0300

    it will initialize migrate

--- manage.py
+++ manage.py
@@ -1,4 +1,9 @@
-from app import create_app,db
-# .....
+from app import create_app
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User
 # ...
+
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+#......
